0f999471f564f5741aaed0d45a38502132554337
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-01-22 20:48:59 +0100

    Updated dependencies and started with the theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+import "@fontsource-variable/dm-sans";
+import { MantineProvider, createTheme } from "@mantine/core";
+import "@mantine/core/styles.css";
 import { Provider } from "jotai";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const theme = createTheme({
+    fontFamily: "DM Sans Variable, sans-serif",
+    defaultRadius: "lg",
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <Provider>
-            <App />
+            <MantineProvider theme={theme}>
+                <App />
+            </MantineProvider>
         </Provider>
     </React.StrictMode>
 );
