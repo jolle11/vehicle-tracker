283f271c353faab3d1a5d988743f582c2e848d23
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-01-28 11:41:37 +0100

    WIP - Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,34 +10,59 @@ import {
     TextInput,
     Title,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    const form = useForm({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    const handleLogin = () => {};
+
     return (
         <Container size={420} my={40}>
             <Title ta="center">Welcome back!</Title>
             <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                <TextInput label="Email" placeholder="you@gmail.com" required />
-                <PasswordInput
-                    label="Password"
-                    placeholder="Your password"
-                    required
-                    mt="md"
-                />
-                <Group justify="space-between" mt="lg">
+                <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <TextInput
+                        label="Email"
+                        placeholder="you@gmail.com"
+                        required
+                        {...form.getInputProps("email")}
+                    />
+                    <PasswordInput
+                        label="Password"
+                        placeholder="Your password"
+                        required
+                        mt="md"
+                        {...form.getInputProps("password")}
+                    />
+                    {/* <Group justify="space-between" mt="lg">
                     <Checkbox label="Remember me" />
-                    {/* <Anchor component="button" size="sm">
+                    <Anchor component="button" size="sm">
                         Forgot password?
-                    </Anchor> */}
-                </Group>
-                <Button fullWidth mt="xl">
-                    Login
-                </Button>
-                <Text c="dimmed" size="sm" ta="center" mt={5}>
-                    Do not have an account yet?&nbsp;
-                    <Anchor size="sm" component="button">
-                        Register
                     </Anchor>
-                </Text>
+                </Group> */}
+                    <Button fullWidth mt="xl" type="submit">
+                        Login
+                    </Button>
+                    <Text c="dimmed" size="sm" ta="center" mt={5}>
+                        Do not have an account yet?&nbsp;
+                        <Anchor
+                            size="sm"
+                            component="button"
+                            onClick={() => navigate("/register")}
+                        >
+                            Register
+                        </Anchor>
+                    </Text>
+                </form>
             </Paper>
         </Container>
     );
