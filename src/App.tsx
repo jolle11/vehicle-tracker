7ab7fc025fd0889c5f0ae713213e67dcd1934502
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-01-29 21:40:44 +0100

    Added route

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-    return (
-        <div className="app">
-            <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<DashboardPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
-                </Routes>
-            </Router>
-        </div>
-    );
+	return (
+		<div className="app">
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<DashboardPage />} />
+					<Route path="/dashboard" element={<DashboardPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
