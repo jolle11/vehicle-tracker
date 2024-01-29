5a4ff58f41d29d4169e14d7b2db798ca954c06b7
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-01-29 21:44:27 +0100

    WIP - Register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,95 +1,97 @@
 import {
-    Anchor,
-    Button,
-    Container,
-    Paper,
-    PasswordInput,
-    Text,
-    TextInput,
-    Title,
+	Anchor,
+	Button,
+	Container,
+	Paper,
+	PasswordInput,
+	Text,
+	TextInput,
+	Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useNavigate } from "react-router-dom";
+import { useRegisterUser } from "../auth";
 
 const RegisterPage = () => {
-    const navigate = useNavigate();
-    const form = useForm({
-        initialValues: {
-            username: "",
-            email: "",
-            emailVisibility: true,
-            password: "",
-            passwordConfirm: "",
-            name: "",
-            surname: "",
-        },
-    });
+	const navigate = useNavigate();
+	const form = useForm({
+		initialValues: {
+			username: "",
+			email: "",
+			emailVisibility: true,
+			password: "",
+			passwordConfirm: "",
+			name: "",
+			surname: "",
+		},
+	});
 
-    const handleRegister = form.onSubmit(
-        (values) => console.log(values),
-        (errors) => console.error(errors)
-    );
+	const handleRegister = form.onSubmit((values) =>
+		useRegisterUser(values)
+			.then(() => navigate("/dashboard"))
+			.catch((error) => console.log(error.message)),
+	);
 
-    return (
-        <Container size={420} my={40}>
-            <Title ta="center">Let's start tracking</Title>
-            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                <form onSubmit={handleRegister}>
-                    <TextInput
-                        label="Alias"
-                        placeholder="Your alias"
-                        required
-                        {...form.getInputProps("username")}
-                    />
-                    <TextInput
-                        label="Email"
-                        placeholder="you@gmail.com"
-                        required
-                        {...form.getInputProps("email")}
-                    />
-                    <TextInput
-                        label="Name"
-                        placeholder="Your name"
-                        required
-                        mt="md"
-                        {...form.getInputProps("name")}
-                    />
-                    <TextInput
-                        label="Surname"
-                        placeholder="Your surname"
-                        required
-                        {...form.getInputProps("surname")}
-                    />
-                    <PasswordInput
-                        label="Password"
-                        placeholder="Your password"
-                        required
-                        mt="md"
-                        {...form.getInputProps("password")}
-                    />
-                    <PasswordInput
-                        label="PasswordConfirm"
-                        placeholder="Confirm your password"
-                        required
-                        {...form.getInputProps("passwordConfirm")}
-                    />
-                    <Button fullWidth mt="xl" type="submit">
-                        Register
-                    </Button>
-                    <Text c="dimmed" size="sm" ta="center" mt={5}>
-                        Already have an account yet?&nbsp;
-                        <Anchor
-                            size="sm"
-                            component="button"
-                            onClick={() => navigate("/login")}
-                        >
-                            Login
-                        </Anchor>
-                    </Text>
-                </form>
-            </Paper>
-        </Container>
-    );
+	return (
+		<Container size={420} my={40}>
+			<Title ta="center">Let's start tracking</Title>
+			<Paper withBorder shadow="md" p={30} mt={30} radius="md">
+				<form onSubmit={handleRegister}>
+					<TextInput
+						label="Alias"
+						placeholder="Your alias"
+						required
+						{...form.getInputProps("username")}
+					/>
+					<TextInput
+						label="Email"
+						placeholder="you@gmail.com"
+						required
+						{...form.getInputProps("email")}
+					/>
+					<TextInput
+						label="Name"
+						placeholder="Your name"
+						required
+						mt="md"
+						{...form.getInputProps("name")}
+					/>
+					<TextInput
+						label="Surname"
+						placeholder="Your surname"
+						required
+						{...form.getInputProps("surname")}
+					/>
+					<PasswordInput
+						label="Password"
+						placeholder="Your password"
+						required
+						mt="md"
+						{...form.getInputProps("password")}
+					/>
+					<PasswordInput
+						label="Password confirmation"
+						placeholder="Confirm your password"
+						required
+						{...form.getInputProps("passwordConfirm")}
+					/>
+					<Button fullWidth mt="xl" type="submit">
+						Register
+					</Button>
+					<Text c="dimmed" size="sm" ta="center" mt={5}>
+						Already have an account yet?&nbsp;
+						<Anchor
+							size="sm"
+							component="button"
+							onClick={() => navigate("/login")}
+						>
+							Login
+						</Anchor>
+					</Text>
+				</form>
+			</Paper>
+		</Container>
+	);
 };
 
 export default RegisterPage;
