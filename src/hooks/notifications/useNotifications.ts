ff4d9f9f5b795be4023f308b7a5d86c2d25edca9
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-02-09 17:05:49 +0100

    WIP - useNotifications hook

--- src/hooks/notifications/useNotifications.ts
+++ src/hooks/notifications/useNotifications.ts
@@ -16,16 +16,16 @@ export const useNotifications = () => {
 					title: "Success",
 					message: notification.message,
 					color: "green",
-					icon: notification.icon,
-					autoClose: 2000,
+					icon: notification.icon ?? null,
+					autoClose: notification.time ?? 2000,
 				});
 			case "error":
 				return notifications.show({
 					title: "Oops",
 					message: notification.message,
 					color: "red",
-					icon: notification.icon,
-					autoClose: 2000,
+					icon: notification.icon ?? null,
+					autoClose: notification.time ?? 2000,
 				});
 		}
 	};
