d0867a26e1ad76b2aa2ec72699bbd9c9532e06b5
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-02-14 22:32:33 +0100

    WIP - Dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,29 @@
+import { useAtom } from "jotai";
+import { userAtom, userVehiclesAtom } from "../atoms/user";
+import { Title } from "@mantine/core";
+import { userAuthenticatedAtom } from "../atoms/auth";
+import { useEffect } from "react";
+import { useListVehicles } from "../hooks/vehicles/useListVehicles";
+
 const DashboardPage = () => {
-	return <div>Your cars will appear here :D</div>;
+	const [user] = useAtom(userAtom);
+	const [userAuthenticated] = useAtom(userAuthenticatedAtom);
+
+	const [userVehicles, setUserVehicles] = useAtom(userVehiclesAtom);
+
+	const listVehicles = useListVehicles();
+
+	useEffect(() => {
+		if (userAuthenticated) {
+			listVehicles(user.id).then((response) => setUserVehicles(response.items));
+		}
+	}, []);
+
+	return userAuthenticated ? (
+		<Title>Hello {user.username}!</Title>
+	) : (
+		<Title>Take control of your car expenses with vehicle tracker</Title>
+	);
 };
 
 export default DashboardPage;
