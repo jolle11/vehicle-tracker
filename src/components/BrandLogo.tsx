0bd22a3c7f73252b8b94acc75e3e3db185e4d975
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-02-17 20:56:39 +0100

    Brand logo component

---
+++ src/components/BrandLogo.tsx
@@ -0,0 +1,19 @@
+import { Image } from "@mantine/core";
+
+interface IBrandLogo {
+	brand: string;
+}
+
+const BrandLogo = ({ brand }: IBrandLogo) => {
+	const formattedBrand = brand.trim().toLowerCase().replace(" ", "-");
+
+	return (
+		<Image
+			h={100}
+			my={"sm"}
+			src={`https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/thumb/${formattedBrand}.png`}
+		/>
+	);
+};
+
+export default BrandLogo;
