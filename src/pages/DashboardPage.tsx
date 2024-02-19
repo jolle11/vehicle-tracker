4752160927c4aa7275c2048d731270ccd983a835
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-02-19 22:33:26 +0100

    Used VehicleCard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -15,6 +15,7 @@ import { useEffect } from "react";
 import { useListVehicles } from "../hooks/vehicles/useVehicleActions";
 import BrandLogo from "../components/BrandLogo";
 import { GasTankDroplet, Wrench } from "iconoir-react";
+import VehicleCard from "../components/VehicleCard";
 
 const DashboardPage = () => {
 	const [user] = useAtom(userAtom);
@@ -44,39 +45,7 @@ const DashboardPage = () => {
 			<Title>Hello {user.username}!</Title>
 			<Flex gap={"sm"} my={"xl"} justify={"center"} wrap={"wrap"}>
 				{userVehicles.length ? (
-					userVehicles.map((vehicle) => (
-						<Card
-							key={vehicle.id}
-							shadow="sm"
-							padding="lg"
-							radius="md"
-							withBorder
-							w={220}
-						>
-							<Group justify="center">
-								<BrandLogo brand={vehicle.brand} />
-							</Group>
-							<Group justify="space-between" mb="sm" mt="md" h={30}>
-								<Title size={20}>{vehicle.nameplate}</Title>
-								<Badge
-									color={`#${vehicle.color}`}
-									style={{ boxShadow: "0px 2px 5px gray" }}
-								>
-									&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								</Badge>
-							</Group>
-							<Group justify="center">
-								<Button color="blue" my="sm" variant="light">
-									{/* TODO Add modal to add new fuel register to the car */}
-									<GasTankDroplet />
-								</Button>
-								<Button color="blue" my="sm" variant="light">
-									{/* TODO Add modal to add new repair register to the car */}
-									<Wrench />
-								</Button>
-							</Group>
-						</Card>
-					))
+					userVehicles.map((vehicle) => <VehicleCard vehicle={vehicle} />)
 				) : (
 					<Loader color="blue" size={"xl"} mt={"xl"} />
 				)}
