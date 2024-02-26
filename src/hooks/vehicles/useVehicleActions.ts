37058922fd09868f8329327dfdd67decbcbec60a
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-02-26 19:55:17 +0100

    Changed collection

--- src/hooks/vehicles/useVehicleActions.ts
+++ src/hooks/vehicles/useVehicleActions.ts
@@ -4,7 +4,7 @@ export const useListVehicles = () => {
 	const pb = useConnectDB();
 	return async (id: string) => {
 		return (await pb)
-			.collection("vehicles")
+			.collection("avg_kms_and_paid")
 			.getList(1, 20, { filter: `user_id = "${id}"` });
 	};
 };
