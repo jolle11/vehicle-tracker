e33def0b12c0b01c77c52b56b1bf311492b161d6
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-03-02 18:59:52 +0100

    km list and small refactor

--- src/hooks/vehicles/useVehicleActions.ts
+++ src/hooks/vehicles/useVehicleActions.ts
@@ -2,9 +2,19 @@ import { useConnectDB } from "../auth/useConnectDB";
 
 export const useListVehicles = () => {
 	const pb = useConnectDB();
-	return async (id: string) => {
+	return async (userId: string) => {
 		return (await pb)
 			.collection("avg_kms_and_paid")
-			.getList(1, 20, { filter: `user_id = "${id}"` });
+			.getList(1, 20, { filter: `user_id = "${userId}"` });
+	};
+};
+
+export const useListKms = () => {
+	const pb = useConnectDB();
+	return async (vehicleId: string) => {
+		return (await pb).collection("kms").getList(1, 500, {
+			sort: "-created",
+			filter: `vehicle_id = "${vehicleId}"`,
+		});
 	};
 };
