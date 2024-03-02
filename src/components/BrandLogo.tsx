edfca0711c0ed4e9b5b6def9cca39909976fc153
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-03-02 18:59:19 +0100

    Added page prop for better logo display

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -2,14 +2,15 @@ import { Image } from "@mantine/core";
 
 interface IBrandLogo {
 	brand: string;
+	page?: boolean;
 }
 
-const BrandLogo = ({ brand }: IBrandLogo) => {
+const BrandLogo = ({ brand, page = false }: IBrandLogo) => {
 	const formattedBrand = brand.trim().toLowerCase().replace(" ", "-");
 
 	return (
 		<Image
-			h={100}
+			h={page ? undefined : 100}
 			fit="contain"
 			my={"sm"}
 			src={`https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/thumb/${formattedBrand}.png`}
