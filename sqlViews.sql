bfdc7e2fed6af059629c95bac1be781417a6653e
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-03-03 19:39:16 +0100

    Fixed media_tank

--- sqlViews.sql
+++ sqlViews.sql
@@ -6,14 +6,14 @@ SELECT
   v.brand,
   v.nameplate,
   v.color,
-  ROUND(AVG(t.km_diff), 2) AS media_tank,
+  COALESCE(ROUND(AVG(t.last_km - t.prev_km), 2), 0) AS media_tank,
   ROUND(AVG(t.paid), 2) AS media_paid,
   t.last_km
 FROM (
   SELECT
     vehicle_id,
     km AS last_km,
-    (km - LAG(km) OVER (PARTITION BY vehicle_id ORDER BY id)) AS km_diff,
+    LAG(km) OVER (PARTITION BY vehicle_id ORDER BY created DESC) AS prev_km,
     paid
   FROM kms
 ) AS t
