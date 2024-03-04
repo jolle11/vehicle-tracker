38d160ecb51b92bac6aa3b57957d052189c70e35
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-03-04 23:54:46 +0100

    New modals and functionalities

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,13 +1,28 @@
 import { useAtom } from "jotai";
 import { userAtom, userVehiclesAtom } from "../atoms/user";
-import { Container, Title, Flex, Loader, Modal } from "@mantine/core";
+import {
+	Container,
+	Title,
+	Flex,
+	Loader,
+	Modal,
+	Button,
+	Paper,
+	TextInput,
+	ColorInput,
+	NumberInput,
+} from "@mantine/core";
 import { userAuthenticatedAtom } from "../atoms/auth";
 import { useEffect, useState } from "react";
-import { useListVehicles } from "../hooks/vehicles/useVehicleActions";
+import {
+	useCreateVehicle,
+	useListVehicles,
+	useRegisterKms,
+} from "../hooks/vehicles/useVehicleActions";
 import VehicleCard from "../components/VehicleCard";
-import { useDisclosure } from "@mantine/hooks";
 import useNumberFormat from "../hooks/utils/useNumberFormat";
 import NewVehicleCard from "../components/NewVehicleCard";
+import { useForm } from "@mantine/form";
 
 const DashboardPage = () => {
 	const [user] = useAtom(userAtom);
@@ -16,15 +31,41 @@ const DashboardPage = () => {
 
 	const listVehicles = useListVehicles();
 	const mediaFormat = useNumberFormat();
+	const createVehicle = useCreateVehicle();
+	const registerKm = useRegisterKms();
 
 	const [loading, setLoading] = useState(true);
+	const [list, setList] = useState(false);
 
-	const [opened, { open, close }] = useDisclosure(false);
+	const [open, setOpen] = useState("");
+
+	const createForm = useForm({
+		initialValues: {
+			user_id: "",
+			brand: "",
+			nameplate: "",
+			color: "",
+			current_kms: 0,
+		},
+	});
+
+	const handleNewVehicle = createForm.onSubmit((values) => {
+		createVehicle(values).then((response) => {
+			registerKm({ vehicle_id: response.id, km: values.current_kms });
+			setLoading(true);
+			setOpen("");
+			setTimeout(() => {
+				setList(!list);
+			}, 1000);
+			createForm.reset();
+		});
+	});
 
 	useEffect(() => {
 		if (userAuthenticated) {
 			listVehicles(user.id)
 				.then((response) => {
+					console.log("In");
 					const formatResponse = response.items.map((item) => ({
 						id: item.id,
 						nameplate: item.nameplate,
@@ -36,40 +77,93 @@ const DashboardPage = () => {
 					}));
 					setUserVehicles(formatResponse);
 					setLoading(false);
+					createForm.setFieldValue("user_id", user.id);
 				})
 				.catch((error) => console.log(error));
 		}
-	}, [user]);
+	}, [user, list]);
 
 	return userAuthenticated ? (
 		<Container size={"lg"} my={"xl"}>
 			<Flex my={"xl"} align={"center"} direction={"column"} wrap={"wrap"}>
 				<Title>Hello {user.username}!</Title>
 				<Flex gap={"md"} my={"xl"} justify={"center"} wrap={"wrap"}>
-					{/* TODO Think switch to open different modals */}
 					{loading && <Loader size={"xl"} mt={"xl"} />}
-					{!(loading || userVehicles.length) && <NewVehicleCard />}
+					{!(loading || userVehicles.length) && (
+						<NewVehicleCard openModal={setOpen} />
+					)}
 					{!loading && !!userVehicles.length && (
 						<>
 							{userVehicles.map((vehicle) => (
 								<VehicleCard
 									key={vehicle.id}
 									vehicle={vehicle}
-									openModal={open}
+									openModal={setOpen}
 								/>
 							))}
-							<NewVehicleCard />
+							<NewVehicleCard openModal={setOpen} />
 						</>
 					)}
 				</Flex>
 			</Flex>
-			<Modal opened={opened} onClose={close} title="New register">
-				- fuel or repair BUTTON <br />- km amount <br />- paid <br />- e/litre
-				<br />- comment
+			{/* TODO Separate modals in different components */}
+			<Modal
+				opened={open === "add-fuel"}
+				onClose={() => setOpen("")}
+				title="Add fuel register"
+			>
+				- vehicle_id <br />- km <br />- paid <br />- price
+			</Modal>
+			<Modal
+				opened={open === "add-new-car"}
+				onClose={() => setOpen("")}
+				title="Add new vehicle"
+				centered
+			>
+				<Paper px={20} pb={20} radius="md">
+					<form onSubmit={handleNewVehicle}>
+						{/* TODO Change to autocomplete with option to send text input if nothing found */}
+						<TextInput
+							label="Brand"
+							placeholder="Choose your brand"
+							required
+							{...createForm.getInputProps("brand")}
+						/>
+						<TextInput
+							label="Nameplate"
+							placeholder="0000ABC"
+							required
+							{...createForm.getInputProps("nameplate")}
+							mt={10}
+						/>
+						<ColorInput
+							label="Color"
+							placeholder="Vehicle color"
+							required
+							{...createForm.getInputProps("color")}
+							mt={10}
+						/>
+						<NumberInput
+							label="Current Kms"
+							placeholder="123456"
+							required
+							allowNegative={false}
+							{...createForm.getInputProps("currentkms")}
+							mt={10}
+						/>
+						<Button fullWidth mt="lg" type="submit" disabled={loading ?? true}>
+							{loading ? <Loader color="blue" size={"sm"} /> : "Add"}
+						</Button>
+					</form>
+				</Paper>
 			</Modal>
 		</Container>
 	) : (
-		<Title>Take control of your vehicle/s</Title>
+		<Container size={"lg"} my={"xl"}>
+			<Flex my={"xl"} align={"center"} direction={"column"} wrap={"wrap"}>
+				<Title>Take control of your vehicle/s</Title>
+			</Flex>
+		</Container>
 	);
 };
 
