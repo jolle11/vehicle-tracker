56afa9f49845281b43ae88eedc7f36ce17bcae6a
Jordi Ollé Ballesté <jordiolle11@gmail.com>
2024-03-06 12:17:35 +0100

    Small logic change

--- sqlViews.sql
+++ sqlViews.sql
@@ -6,15 +6,16 @@ SELECT
   v.brand,
   v.nameplate,
   v.color,
-  COALESCE(ROUND(AVG(t.last_km - t.prev_km), 2), 0) AS media_tank,
-  ROUND(AVG(t.paid), 2) AS media_paid,
+  COALESCE(ROUND(AVG(CASE WHEN t.row_num > 1 THEN t.last_km - t.prev_km END), 2), 0) AS media_tank,
+  COALESCE(ROUND(AVG(CASE WHEN t.row_num > 1 THEN t.paid END), 2), 0) AS media_paid,
   t.last_km
 FROM (
   SELECT
     vehicle_id,
     km AS last_km,
     LAG(km) OVER (PARTITION BY vehicle_id ORDER BY created DESC) AS prev_km,
-    paid
+    paid,
+    ROW_NUMBER() OVER (PARTITION BY vehicle_id ORDER BY created) AS row_num
   FROM kms
 ) AS t
 JOIN vehicles AS v ON t.vehicle_id = v.id
